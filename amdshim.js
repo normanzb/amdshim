e2b92b46eb22aeafd60a20268a78f604e0632bda
Norman Xu <homyu.shinn@gmail.com>
2014-04-03 12:16:27 +0100

    upgrade the promise

--- amdshim.js
+++ amdshim.js
@@ -4,90 +4,91 @@ var require, define;
     /*
      * A compact version of Promise
      */
-    function Promise() {
-        this._successCallbacks = [];
-        this._failureCallbacks = [];
-    }
+    var Promise = (function(){
+        var PROTOTYPE = 'prototype', FUNCTION = 'function', RESOLVED = 'resolved', REJECTED = 'rejected';
+        function Promise() {
+            this._s = [];
+            this._f = [];
+        }
+        Promise[PROTOTYPE].then = function(onSuccess, onFailure) {
+            var p = new Promise();
+            var me = this;
 
-    Promise.prototype.then = function(onSuccess, onFailure) {
-        var p;
-        var me = this;
-        p = new Promise();
+            if (typeof onSuccess == FUNCTION){
+                var handleSuccess = function () {
+                    var res = onSuccess.apply(me, arguments);
+                    if (res && typeof res.then === FUNCTION){
+                        res.then(function(){
+                            p.resolve.apply(p, arguments);
+                        });
+                    }
+                    else{
+                        p.resolve.apply(p, (res == null ? [] : [res]));
+                    }
+                };
 
-        if (typeof onSuccess == 'function'){
-            var handleSuccess = function () {
-                var res = onSuccess.apply(me, arguments);
-                if (res && typeof res.then === 'function'){
-                    res.then(function(){
-                        p.resolve.apply(p, arguments);
-                    });
+                if (me[RESOLVED]) {
+                    handleSuccess.call(null, me.result);
+                } else {
+                    me._s.push(handleSuccess);
                 }
-                else{
-                    p.resolve.apply(p, (res == null ? [] : [res]));
-                }
-            };
-
-            if (me._isSuccessed) {
-                handleSuccess.call(null, me.result);
-            } else {
-                me._successCallbacks.push(handleSuccess);
             }
-        }
 
-        if (typeof onFailure == 'function'){
-            var handleFail = function () {
-                var res = onFailure.apply(me, arguments);
-                if (res && typeof res.then === 'function'){
-                    res.then(function(){
-                        p.resolve.apply(p, arguments);
-                    });
-                }
-                else{
-                    p.resolve.apply(p, (res == null ? [] : [res]));
-                }
-            };
+            if (typeof onFailure == FUNCTION){
+                var handleFail = function () {
+                    var res = onFailure.apply(me, arguments);
+                    if (res && typeof res.then === FUNCTION){
+                        res.then(function(){
+                            p.resolve.apply(p, arguments);
+                        });
+                    }
+                    else{
+                        p.resolve.apply(p, (res == null ? [] : [res]));
+                    }
+                };
 
-            if (me._isFailed) {
-                handleFail.call(null, me.error);
-            } else {
-                me._failureCallbacks.push(handleFail);
+                if (me[REJECTED]) {
+                    handleFail.call(null, me.error);
+                } else {
+                    me._f.push(handleFail);
+                }
             }
-        }
-        
-        return p;
-    };
+            
+            return p;
+        };
 
-    Promise.prototype.resolve = function() {
-        var me = this;
+        Promise[PROTOTYPE].resolve = function() {
+            var me = this;
 
-        me.result = arguments[0];
-        
-        if (me._isFailed){
-            return;
-        }
+            me.result = arguments[0];
+            
+            if (me[REJECTED]){
+                return;
+            }
 
-        me._isSuccessed = true;
-        for (var i = 0; i < me._successCallbacks.length; i++) {
-            me._successCallbacks[i].call(null, me.result);
-        }
-        me._successCallbacks = [];
-    };
+            me[RESOLVED] = true;
+            for (var i = 0; i < me._s.length; i++) {
+                me._s[i].call(null, me.result);
+            }
+            me._s = [];
+        };
+        Promise[PROTOTYPE].reject = function(){
+            var me = this;
 
-    Promise.prototype.reject = function(){
-        var me = this;
+            me.error = arguments[0];
 
-        me.error = arguments[0];
+            if (me[RESOLVED]){
+                return;
+            }
 
-        if (me._isSuccessed){
-            return;
-        }
-
-        me._isFailed = true;
-        for (var i = 0; i < me._failureCallbacks.length; i++) {
-            me._failureCallbacks[i].call(null, me.error);
-        }
-        me._failureCallbacks = [];
-    };
+            me[REJECTED] = true;
+            for (var i = 0; i < me._f.length; i++) {
+                me._f[i].call(null, me.error);
+            }
+            me._f = [];
+        };
+        return Promise;
+    })();
 
     /* 
      * AMD shim 
